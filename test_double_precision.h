154d7a194a713c0b46966f162e2b4ca4a8fd80db
s-malchenko <malchenko.sg@gmail.com>
2019-09-18 21:10:03 +0300

    Part E fully completed.

--- test_double_precision.h
+++ test_double_precision.h
@@ -2,7 +2,7 @@
 
 #include <cmath>
 
-inline bool doublesEqual(long double lhs, long double rhs, long double precision = 0.0001)
+inline bool doublesEqual(long double lhs, long double rhs, long double precision = 0.01)
 {
     return std::abs(lhs - rhs) <= precision;
 }
